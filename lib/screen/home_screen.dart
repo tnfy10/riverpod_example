d28bfb53120a6c412abf22ab94c758fe9955a697
Hong Sunghwi <tnfy10@gmail.com>
2022-12-17 16:51:12 +0900

    provider 안에 provider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_example/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_example/screen/family_modifier_screen.dart';
 import 'package:riverpod_example/screen/future_provider_screen.dart';
 import 'package:riverpod_example/screen/listen_provider_screen.dart';
+import 'package:riverpod_example/screen/provider_screen.dart';
 import 'package:riverpod_example/screen/select_provider_screen.dart';
 import 'package:riverpod_example/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_example/screen/state_provider_screen.dart';
@@ -83,6 +84,14 @@ class HomeScreen extends StatelessWidget {
                           builder: (_) => const SelectProviderScreen()));
                 },
                 child: const Text('SelectProviderScreen')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const ProviderScreen()));
+                },
+                child: const Text('ProviderScreen')),
           ],
         ));
   }
