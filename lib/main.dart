8d7a83b6ba7940776b66a889539de2a73ba37349
Hong Sunghwi <tnfy10@gmail.com>
2022-12-17 17:02:22 +0900

    provider observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example/riverpod/provider_observer.dart';
 import 'package:riverpod_example/screen/home_screen.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MaterialApp(home: HomeScreen())));
+  runApp(ProviderScope(observers: [
+    Logger(),
+  ], child: const MaterialApp(home: HomeScreen())));
 }
